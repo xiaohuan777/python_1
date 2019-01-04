08428e9fb7cef1f20b44927ad665cf5888782520
肖欢 <xiaohuan@tsj-004841.ytx.com>
2019-01-04 09:49:15 +0800

    commit

--- python_files/selenium_python/base/main.py
+++ python_files/selenium_python/base/main.py
@@ -1,7 +1,7 @@
 # coding:utf-8
 
 import sys
-sys.path.append(r'D:\python_files\selenium_python')
+sys.path.append('/Users/xiaohuan/Desktop/face/xiaohuan/python/python_files/selenium_python')
 from util.get_excel import ExcelOperate
 from util.driver import ActionDriver
 
